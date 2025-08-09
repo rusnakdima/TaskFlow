bf2ea9577e81281704419cf8e2ba2286480f9b16
Dmitriy303 <rusnakdima03@gmail.com>
2025-08-09 18:55:12 +0500

    Added bottom navigation to main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,29 @@
 /* sys lib */
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { RouterOutlet } from "@angular/router";
+import { Router, RouterOutlet } from "@angular/router";
 
 /* components */
 import { HeaderComponent } from "@components/header/header.component";
 import { WindowNotifyComponent } from "@components/window-notify/window-notify.component";
+import { BottomNavComponent } from "@components/bottom-nav/bottom-nav.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, WindowNotifyComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, WindowNotifyComponent, BottomNavComponent],
   templateUrl: "./app.component.html",
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     const theme = localStorage.getItem("theme") ?? "";
     document.querySelector("html")!.setAttribute("class", theme);
+
+    const token = localStorage.getItem("token") ?? "";
+    if (!token) {
+      this.router.navigate(["/login"]);
+    }
   }
 }
