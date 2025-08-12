08b1b2612fe2b61b071ec45c2469666406a290e7
Dmitriy303 <rusnakdima03@gmail.com>
2025-08-12 16:01:54 +0500

    Changed data on page

--- src/app/views/profile/profile.component.ts
+++ src/app/views/profile/profile.component.ts
@@ -1,8 +1,51 @@
-import { Component } from "@angular/core";
+/* sys lib */
+import { CommonModule } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
+import { RouterModule } from "@angular/router";
+
+/* materials */
+import { MatIconModule } from "@angular/material/icon";
+
+/* models */
+import { Profile } from "@models/profile";
+import { Response, ResponseStatus } from "@models/response";
+
+/* services */
+import { AuthService } from "@services/auth.service";
+import { NotifyService } from "@services/notify.service";
+import { ProfileService } from "@services/profile.service";
 
 @Component({
   selector: "app-profile",
-  imports: [],
+  standalone: true,
+  providers: [AuthService, ProfileService, NotifyService],
+  imports: [CommonModule, RouterModule, MatIconModule],
   templateUrl: "./profile.component.html",
 })
-export class ProfileComponent {}
+export class ProfileComponent implements OnInit {
+  constructor(
+    private authService: AuthService,
+    private profileService: ProfileService,
+    private notifyService: NotifyService
+  ) {}
+
+  profile: Profile | null = null;
+
+  ngOnInit(): void {
+    const userId = this.authService.getValueByKey("id");
+    if (userId && userId != "") {
+      this.profileService
+        .get_by_user_id<Profile>(userId)
+        .then((response: Response<Profile>) => {
+          if (response.status === ResponseStatus.SUCCESS) {
+            this.profile = response.data;
+          }
+        })
+        .catch((err: Response<string>) => {
+          if (err.status === ResponseStatus.ERROR) {
+            this.notifyService.showError(err.message ?? err.toString());
+          }
+        });
+    }
+  }
+}
