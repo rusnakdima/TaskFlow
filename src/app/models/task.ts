4db0b06a0cce8963c83d00d20dda366d74fd9e37
Dmitriy303 <rusnakdima03@gmail.com>
2025-08-14 00:59:19 +0500

    Changed enum & model task

--- src/app/models/task.ts
+++ src/app/models/task.ts
@@ -1,10 +1,11 @@
 /* models */
+import { Subtask } from "./subtask";
 import { Todo } from "./todo";
 
 export enum PriorityTask {
-  Low,
-  Medium,
-  High,
+  LOW = 'low',
+  MEDIUM = 'medium',
+  HIGH = 'high',
 }
 
 export interface Task {
@@ -12,6 +13,7 @@ export interface Task {
   todo: Todo;
   title: string;
   description: string;
+  subtasks: Array<Subtask>;
   isCompleted: boolean;
   priority: PriorityTask;
   createdAt: string;
