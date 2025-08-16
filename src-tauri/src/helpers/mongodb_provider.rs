0936a45091402573451602fc121f3f61ef8a15dd
Dmitriy303 <rusnakdima03@gmail.com>
2025-08-16 19:48:05 +0500

    Changed logic get data from relationship tables

--- src-tauri/src/helpers/mongodb_provider.rs
+++ src-tauri/src/helpers/mongodb_provider.rs
@@ -1,25 +1,25 @@
 /* sys lib */
 use dotenv::dotenv;
 use mongodb::{
-  bson::{doc, from_document, Document, Uuid},
+  bson::{doc, Document, Uuid},
   Client, Collection, Database,
 };
-use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
 use std::env;
-
-/* models */
-use crate::models::list_models::ListFullModels;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[allow(non_snake_case)]
 pub enum TypesField {
-  One,
-  Many,
+  OneToOne,
+  OneToMany,
+  ManyToOne,
+  ManyToMany,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[allow(non_snake_case)]
 pub struct RelationObj {
+  pub collection_name: String,
   pub typeField: TypesField,
   pub nameField: String,
   pub newNameField: String,
@@ -40,135 +40,131 @@ impl MongodbProvider {
     }
   }
 
-  pub async fn connect_to_db(&self) -> Result<Database, Box<dyn std::error::Error>> {
+  #[allow(non_snake_case)]
+  pub async fn connectToDB(&self) -> Result<Database, Box<dyn std::error::Error>> {
     let client = Client::with_uri_str(&self.uri).await?;
     Ok(client.database(&self.database))
   }
 
-  pub async fn get_collection<T>(
+  #[allow(non_snake_case)]
+  pub async fn getCollection(
     &self,
     collection_name: &str,
-  ) -> Result<Collection<T>, Box<dyn std::error::Error>>
-  where
-    T: DeserializeOwned + Unpin + Send + Sync,
-  {
-    let db = self.connect_to_db().await?;
-    let collection_doc = db.collection::<T>(collection_name);
+  ) -> Result<Collection<Document>, Box<dyn std::error::Error>> {
+    let db = self.connectToDB().await?;
+    let collection_doc = db.collection::<Document>(collection_name);
     Ok(collection_doc)
   }
 
   #[allow(non_snake_case)]
-  pub async fn get_data_relations<T>(
+  pub async fn getDataRelations(
     &self,
-    collection_name: &str,
-    record: &mut Document,
-    fields: &[&str],
-    // relations: Vec<String>,
-  ) -> Result<Document, Box<dyn std::error::Error>>
-  where
-    T: DeserializeOwned + Unpin + Send + Sync,
-  {
-    // for relation in relations {
-    //   match relation.typeField {
-    //     TypesField::One => {
-    //       let collection = self.get_collection(collection_name).await?;
-    //       let uuid_id = Uuid::parse_str(record["id"].as_str().unwrap())?;
-    //       let filter = doc! { "id": uuid_id };
-    //       let mut result = match collection.find_one(filter).await {
-    //         Ok(doc_opt) => match doc_opt {
-    //           Some(doc) => doc,
-    //           None => {
-    //             return Err(Box::new(std::io::Error::new(
-    //               std::io::ErrorKind::NotFound,
-    //               "Document not found",
-    //             )))
-    //           }
-    //         },
-    //         Err(e) => {
-    //           return Err(Box::new(e));
-    //         }
-    //       };
-    //       record.insert(relation.newNameField.clone(), result);
+    mut record: Document,
+    relations: Vec<RelationObj>,
+  ) -> Result<Document, Box<dyn std::error::Error>> {
+    for relation in relations {
+      match relation.typeField {
+        TypesField::OneToOne => {
+          if let Some(value) = record.get(relation.nameField).cloned() {
+            let result = match self
+              .getByField(
+                &relation.collection_name,
+                None,
+                relation.relations,
+                &value.as_str().unwrap().to_string(),
+              )
+              .await
+            {
+              Ok(doc) => doc,
+              Err(_) => continue,
+            };
+            record.insert(relation.newNameField.clone(), result);
+          }
+        }
+        TypesField::OneToMany => {
+          if let Some(value) = record.get("id").cloned() {
+            let result = match self
+              .getAllByField(
+                &relation.collection_name,
+                Some(doc! { relation.nameField: &value.as_str().unwrap().to_string() }),
+                relation.relations,
+              )
+              .await
+            {
+              Ok(doc) => doc,
+              Err(_) => continue,
+            };
+            record.insert(relation.newNameField.clone(), result);
+          }
+        }
+        TypesField::ManyToOne => {
+          if let Ok(value) = record.get_array(relation.nameField).cloned() {
+            let mut listResult: Vec<Document> = vec![];
+            for id in value {
+              let result = match self
+                .getByField(
+                  &relation.collection_name,
+                  None,
+                  relation.relations.clone(),
+                  &id.as_str().unwrap().to_string(),
+                )
+                .await
+              {
+                Ok(doc) => doc,
+                Err(_) => continue,
+              };
+              listResult.push(result);
+            }
+            record.insert(relation.newNameField.clone(), listResult);
+          }
+        }
+        TypesField::ManyToMany => {}
+      }
+    }
 
-    //       // let data_doc = self
-    //       //   .get_by_id(
-    //       //     collection_name,
-    //       //     relation.relations,
-    //       //     &relation.nameField.as_str(),
-    //       //   )
-    //       //   .await?;
-    //       // record.insert(relation.newNameField.clone(), data_doc);
-    //     }
-    //     TypesField::Many => {
-    //       let collection = self.get_collection(collection_name).await?;
-    //       let filter = doc! { relation.nameField.clone(): record[&relation.nameField.clone().as_str()].clone() };
-    //       let mut cursor = collection.find(filter).await?;
-
-    //       let mut results: Vec<Document> = Vec::new();
-    //       while cursor.advance().await? {
-    //         let doc = cursor.deserialize_current()?;
-    //         results.push(doc);
-    //       }
-    //       record.insert(relation.newNameField.clone(), results);
-
-    //       // let list_docs = self
-    //       //   .get_all_by_filter(
-    //       //     collection_name,
-    //       //     doc! { relation.nameField.clone(): record[relation.nameField.clone().as_str()].clone() },
-    //       //     relation.relations,
-    //       //   )
-    //       //   .await?;
-    //       // record.insert(relation.newNameField.to_string(), list_docs);
-    //     }
-    //   };
-    // }
-
-    Ok(record.clone())
+    Ok(record)
   }
 
-  pub async fn get_all<T>(
+  #[allow(non_snake_case)]
+  pub async fn getAllByField(
     &self,
     collection_name: &str,
     filter: Option<Document>,
-    relations: Option<Vec<String>>,
-  ) -> Result<Vec<T>, Box<dyn std::error::Error>>
-  where
-    T: DeserializeOwned + Unpin + Send + Sync,
-  {
-    let collection = self.get_collection::<T>(collection_name).await?;
+    relations: Option<Vec<RelationObj>>,
+  ) -> Result<Vec<Document>, Box<dyn std::error::Error>> {
+    let collection = self.getCollection(collection_name).await?;
     let mut cursor = match filter {
       Some(filter) => collection.find(filter).await?,
       None => collection.find(doc! {}).await?,
     };
 
-    let mut results: Vec<T> = Vec::new();
+    let mut results: Vec<Document> = Vec::new();
     while cursor.advance().await? {
       let doc = cursor.deserialize_current()?;
       results.push(doc);
     }
 
-    // if let Some(relations) = relations {
-    //   for result in &mut results {
-    //     self
-    //       .get_data_relations(collection_name, result, relations.to_vec())
-    //       .await?;
-    //   }
-    // }
+    if let Some(relations) = relations {
+      let mut enriched_results = Vec::new();
+      for result in results {
+        let enriched = Box::pin(self.getDataRelations(result, relations.clone())).await?;
+        enriched_results.push(enriched);
+      }
+      results = enriched_results;
+    }
 
     Ok(results)
   }
 
-  pub async fn get_by_field<T>(
+  #[allow(non_snake_case)]
+  pub async fn getByField(
     &self,
     collection_name: &str,
     filter: Option<Document>,
-    relations: Option<Vec<String>>,
+    relations: Option<Vec<RelationObj>>,
     id: &str,
-  ) -> Result<T, Box<dyn std::error::Error>>
-  where
-    T: DeserializeOwned + Unpin + Send + Sync,
-  {
-    let collection = self.get_collection::<T>(collection_name).await?;
+  ) -> Result<Document, Box<dyn std::error::Error>> {
+    let collection = self.getCollection(collection_name).await?;
     let filter = match filter {
       Some(filter) => filter,
       None => {
@@ -177,7 +173,7 @@ impl MongodbProvider {
       }
     };
 
-    let mut result = match collection.find_one(filter).await {
+    let result = match collection.find_one(filter).await {
       Ok(doc_opt) => match doc_opt {
         Some(doc) => doc,
         None => {
@@ -192,56 +188,47 @@ impl MongodbProvider {
       }
     };
 
-    // if let Some(relations) = relations {
-    //   self
-    //     .get_data_relations(collection_name, &mut result, relations.to_vec())
-    //     .await?;
-    // }
+    let enriched_result = if let Some(relations) = relations {
+      Box::pin(self.getDataRelations(result, relations.clone())).await?
+    } else {
+      result
+    };
 
-    Ok(result)
+    Ok(enriched_result)
   }
 
-  pub async fn create<T>(
+  pub async fn create(
     &self,
     collection_name: &str,
-    data: Document,
-  ) -> Result<bool, Box<dyn std::error::Error>>
-  where
-    T: DeserializeOwned + Unpin + Send + Sync,
-  {
-    let collection = self.get_collection::<Document>(collection_name).await?;
-    collection.insert_one(data).await?;
+    document: Document,
+  ) -> Result<bool, Box<dyn std::error::Error>> {
+    let collection = self.getCollection(collection_name).await?;
+    collection.insert_one(document).await?;
 
     Ok(true)
   }
 
-  pub async fn update<T>(
+  pub async fn update(
     &self,
     collection_name: &str,
     id: &str,
-    data: Document,
-  ) -> Result<bool, Box<dyn std::error::Error>>
-  where
-    T: DeserializeOwned + Unpin + Send + Sync,
-  {
-    let collection = self.get_collection::<T>(collection_name).await?;
+    document: Document,
+  ) -> Result<bool, Box<dyn std::error::Error>> {
+    let collection = self.getCollection(collection_name).await?;
     let uuid_id = Uuid::parse_str(id)?;
     let filter = doc! { "id": uuid_id };
-    let update = doc! { "$set": data };
+    let update = doc! { "$set": document };
     collection.update_one(filter, update).await?;
 
     Ok(true)
   }
 
-  pub async fn delete<T>(
+  pub async fn delete(
     &self,
     collection_name: &str,
     id: &str,
-  ) -> Result<bool, Box<dyn std::error::Error>>
-  where
-    T: DeserializeOwned + Unpin + Send + Sync,
-  {
-    let collection = self.get_collection::<T>(collection_name).await?;
+  ) -> Result<bool, Box<dyn std::error::Error>> {
+    let collection = self.getCollection(collection_name).await?;
     let uuid_id = Uuid::parse_str(id)?;
     let filter = doc! { "id": uuid_id };
     collection.delete_one(filter).await?;
