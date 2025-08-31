49a54a50fd052ce8c76af2f6e863155a5cc700a3
Dmitriy303 <rusnakdima03@gmail.com>
2025-08-31 19:09:12 +0500

    Changed version dependencies; Changed target sdk android

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -15,13 +15,13 @@ val tauriProperties = Properties().apply {
 }
 
 android {
-    compileSdk = 33
+    compileSdk = 34
     namespace = "com.tcs.taskflow"
     defaultConfig {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
         applicationId = "com.tcs.taskflow"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
     }
@@ -74,13 +74,17 @@ rust {
 }
 
 dependencies {
-    implementation("androidx.webkit:webkit:1.14.0")
-    implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.webkit:webkit:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.activity:activity-ktx:1.8.0")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.core:core:1.13.0")
+    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.transition:transition:1.5.0")
+    implementation("androidx.annotation:annotation-experimental:1.4.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.4")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+    androidTestImplementation("androidx.test.ext:junit:1.2.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.0")
 }
 
 apply(from = "tauri.build.gradle.kts")
