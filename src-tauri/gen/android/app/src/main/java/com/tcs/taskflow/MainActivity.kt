2972d16bc50393c5591f8818a521be3019df4203
Dmitriy303 <rusnakdima03@gmail.com>
2025-09-13 23:33:42 +0500

    Fixed space for status bar in android build

--- src-tauri/gen/android/app/src/main/java/com/tcs/taskflow/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/tcs/taskflow/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.tcs.taskflow
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
-    enableEdgeToEdge()
     super.onCreate(savedInstanceState)
   }
 }
