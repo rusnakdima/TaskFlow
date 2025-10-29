6cc1da947a1906539d21cc0336d4d869917d6999
Dmitriy303 <rusnakdima03@gmail.com>
2025-10-29 16:38:41 +0500

    Changed style code

--- src/app/components/fields/checkbox/checkbox.component.ts
+++ src/app/components/fields/checkbox/checkbox.component.ts
@@ -1,38 +1,31 @@
 /* sys lib */
-import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from "@angular/common";
+import { Component, Input } from "@angular/core";
+import { FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 /* materials */
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatCheckboxModule } from "@angular/material/checkbox";
 
 /* models */
-import { CheckboxField } from '@models/form-field';
+import { CheckboxField } from "@models/form-field";
 
 @Component({
-  selector: 'app-checkbox',
+  selector: "app-checkbox",
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatCheckboxModule,
-  ],
-  templateUrl: './checkbox.component.html',
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatCheckboxModule],
+  templateUrl: "./checkbox.component.html",
 })
 export class CheckboxComponent {
   constructor() {}
 
-  @Input() label: string = '';
+  @Input() label: string = "";
   @Input() form!: FormGroup;
   @Input() field!: CheckboxField;
 
   isInvalid(attr: string) {
     return (
-      (this.form.get(attr)?.touched || this.form.get(attr)?.dirty) &&
-      this.form.get(attr)?.errors
+      (this.form.get(attr)?.touched || this.form.get(attr)?.dirty) && this.form.get(attr)?.errors
     );
   }
 }
