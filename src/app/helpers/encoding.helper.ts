d624e5e997ce1152ddb59eb50e973ef29687bf6c
DmitriyRusnak <@d.rusnak>
2026-04-28 15:56:02 +0500

    Format Angular helpers and add encoding helper

---
+++ src/app/helpers/encoding.helper.ts
@@ -0,0 +1,23 @@
+export class EncodingHelper {
+  static base64ToArrayBuffer(base64url: string): ArrayBuffer {
+    if (!base64url) {
+      throw new Error("base64url is undefined or empty");
+    }
+    let base64 = base64url.replace(/-/g, "+").replace(/_/g, "/");
+    const binary = atob(base64);
+    const bytes = new Uint8Array(binary.length);
+    for (let i = 0; i < binary.length; i++) {
+      bytes[i] = binary.charCodeAt(i);
+    }
+    return bytes.buffer;
+  }
+
+  static arrayBufferToBase64(buffer: ArrayBuffer): string {
+    const bytes = new Uint8Array(buffer);
+    let binary = "";
+    for (let i = 0; i < bytes.byteLength; i++) {
+      binary += String.fromCharCode(bytes[i]);
+    }
+    return btoa(binary);
+  }
+}
