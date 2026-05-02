ba8d2e33c7c05b09d15b12a96e832c4d1052b6d0
Dmitriy303 <rusnakdima03@gmail.com>
2026-05-02 19:15:14 +0500

    v0.14.2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  version: "0.14.1",
+  version: "0.14.2",
   gitRepoName: "TaskFlow",
   githubUser: "TechCraft-Solutions",
   nameProduct: "Task Flow",
