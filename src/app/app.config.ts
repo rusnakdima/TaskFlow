ac221bbdb25b1c279ba3aba59602d71e7de656ef
DmitriyRusnak <@d.rusnak>
2026-05-05 17:29:21 +0500

    format(app): format app.config and app.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,30 @@
 /* sys lib */
-import { ApplicationConfig } from "@angular/core";
+import { ApplicationConfig, APP_INITIALIZER } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { provideHttpClient } from "@angular/common/http";
 import { provideNativeDateAdapter } from "@angular/material/core";
 
-/* routes */
+/* app */
 import { routes } from "@app/app.routes";
+import { DataService } from "@services/data/data.service";
+import { DataSyncService } from "@services/data/data-sync.service";
+
+function initializeDataSync(dataSyncService: DataSyncService, dataService: DataService) {
+  return () => dataSyncService.initTauriListeners(dataService);
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideNativeDateAdapter(), provideHttpClient()],
+  providers: [
+    provideRouter(routes),
+    provideNativeDateAdapter(),
+    provideHttpClient(),
+    DataService,
+    DataSyncService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeDataSync,
+      deps: [DataSyncService, DataService],
+      multi: true,
+    },
+  ],
 };
