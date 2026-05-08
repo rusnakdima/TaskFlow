c3368c837a187cf336cb4c33f0a822fcf69a6010
DmitriyRusnak <@d.rusnak>
2026-05-08 16:58:26 +0500

    refactor(frontend): remove unused entity handlers, pagination loader, and api provider

--- src/app/services/core/entity-handlers/subtask.handler.ts
+++
@@ -1,9 +0,0 @@
-import { WritableSignal } from "@angular/core";
-import { BaseHandler } from "./base.handler";
-import { Subtask } from "@models/subtask.model";
-
-export class SubtaskHandler extends BaseHandler<Subtask> {
-  constructor(signal: WritableSignal<Subtask[]>) {
-    super(signal);
-  }
-}
--- src/app/services/core/entity-handlers/task.handler.ts
+++
@@ -1,9 +0,0 @@
-import { WritableSignal } from "@angular/core";
-import { BaseHandler } from "./base.handler";
-import { Task } from "@models/task.model";
-
-export class TaskHandler extends BaseHandler<Task> {
-  constructor(signal: WritableSignal<Task[]>) {
-    super(signal);
-  }
-}
