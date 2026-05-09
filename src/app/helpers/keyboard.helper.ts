435344e66cb6da4f7921cb7bffdfc9a069d21378
Dmitriy303 <rusnakdima03@gmail.com>
2026-05-09 21:56:11 +0500

    refactor: update keyboard helper and manage-item view formatting

--- src/app/helpers/keyboard.helper.ts
+++ src/app/helpers/keyboard.helper.ts
@@ -1,9 +1,9 @@
 import { ShortcutService } from "@services/ui/shortcut.service";
-import { Subscription } from "rxjs";
+import { Observable } from "rxjs";
 
 export function bindSaveShortcut(
   shortcutService: ShortcutService,
-  callback: () => void
-): Subscription {
-  return shortcutService.save$.subscribe(callback);
+  _callback: () => void
+): Observable<unknown> {
+  return shortcutService.save$.pipe();
 }
