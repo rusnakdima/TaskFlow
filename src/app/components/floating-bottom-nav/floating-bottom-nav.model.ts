b61471dea6f5cea55e95cbca2de08166b27bce59
Dmitriy303 <rusnakdima03@gmail.com>
2026-05-11 02:54:33 +0500

    refactor(components): update floating-bottom-nav, header and table-view

--- src/app/components/floating-bottom-nav/floating-bottom-nav.model.ts
+++ src/app/components/floating-bottom-nav/floating-bottom-nav.model.ts
@@ -3,4 +3,12 @@ export interface FloatingNavItem {
   icon: string;
   label: string;
   query?: Record<string, string>;
+  routeType?: "todos" | "profile";
+  childRoutes?: NavRouteConfig[];
+}
+
+export interface NavRouteConfig {
+  pattern: RegExp;
+  icon: string;
+  label: string;
 }
