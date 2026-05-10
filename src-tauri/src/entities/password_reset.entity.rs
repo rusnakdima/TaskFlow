3aab91ec72b93279d1ab7db645c5fb7272201b9d
Dmitriy303 <rusnakdima03@gmail.com>
2026-05-10 23:04:14 +0500

    fix(backend): update login, password reset and signup form entities

--- src-tauri/src/entities/password_reset.entity.rs
+++ src-tauri/src/entities/password_reset.entity.rs
@@ -1,25 +1,14 @@
 /* sys lib */
+use nosql_orm::Validate;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, Validate)]
 #[serde(rename_all = "snake_case")]
 pub struct PasswordReset {
+  #[validate(email)]
   pub email: String,
+  #[validate(not_empty)]
   pub code: String,
+  #[validate(not_empty)]
   pub new_password: String,
-}
-
-impl PasswordReset {
-  pub fn validate(&self) -> Result<(), String> {
-    if self.email.is_empty() {
-      return Err("email cannot be empty".to_string());
-    }
-    if self.code.is_empty() {
-      return Err("code cannot be empty".to_string());
-    }
-    if self.new_password.is_empty() {
-      return Err("new_password cannot be empty".to_string());
-    }
-    Ok(())
-  }
 }
