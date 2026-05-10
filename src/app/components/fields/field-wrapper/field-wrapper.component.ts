2707440beb38a97ac0e4330fe78249b026ec830d
Dmitriy303 <rusnakdima03@gmail.com>
2026-05-11 01:53:38 +0500

    refactor(components): remove field-wrapper, filter-control, block-fields, list-toolbar and priority-badge

--- src/app/components/fields/field-wrapper/field-wrapper.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-import { BaseFieldComponent } from "@components/fields/base-field.component";
-
-@Component({
-  selector: "app-field-wrapper",
-  standalone: true,
-  templateUrl: "./field-wrapper.component.html",
-})
-export class FieldWrapperComponent extends BaseFieldComponent {}
