e77f5591f2442d2d43a7e2ff0e1e46b1755b8b0f
Dmitriy303 <rusnakdima03@gmail.com>
2026-05-11 02:15:56 +0500

    refactor(components): update block-fields component

--- src/app/components/form/block-fields/block-fields.component.ts
+++ src/app/components/form/block-fields/block-fields.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, ChangeDetectionStrategy } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { FormGroup } from "@angular/forms";
 
 import { FormField } from "@models/form-field.model";
 
@@ -11,55 +11,41 @@ import { CheckboxComponent } from "@components/fields/checkbox/checkbox.componen
 import { SelectComponent } from "@components/fields/select/select.component";
 import { SlideToggleComponent } from "@components/fields/slide-toggle/slide-toggle.component";
 import { DatePickerComponent } from "@components/fields/date-picker/date-picker.component";
-import { SearchComponent } from "@components/fields/search/search.component";
+import { ImageComponent } from "@components/fields/image/image.component";
+import { RadioComponent } from "@components/fields/radio/radio.component";
+import { SliderComponent } from "@components/fields/slider/slider.component";
+import { SliderRangeComponent } from "@components/fields/slider-range/slider-range.component";
 
 @Component({
   selector: "app-block-fields",
   standalone: true,
   imports: [
     CommonModule,
-    ReactiveFormsModule,
     TextComponent,
-    TextAreaComponent,
     NumberComponent,
-    CheckboxComponent,
-    SelectComponent,
-    SlideToggleComponent,
+    TextAreaComponent,
+    ImageComponent,
     DatePickerComponent,
-    SearchComponent,
+    SlideToggleComponent,
+    SelectComponent,
+    CheckboxComponent,
+    RadioComponent,
+    SliderComponent,
+    SliderRangeComponent,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  template: `
-    @switch (field.type) {
-      @case ("text") {
-        <app-text [parentForm]="parentForm" [formG]="formG" [field]="field" />
-      }
-      @case ("textarea") {
-        <app-text-area [parentForm]="parentForm" [formG]="formG" [field]="field" />
-      }
-      @case ("number") {
-        <app-number [parentForm]="parentForm" [formG]="formG" [field]="field" />
-      }
-      @case ("checkbox") {
-        <app-checkbox [parentForm]="parentForm" [formG]="formG" [field]="field" />
-      }
-      @case ("select") {
-        <app-select [parentForm]="parentForm" [formG]="formG" [field]="field" />
-      }
-      @case ("toggle") {
-        <app-slide-toggle [parentForm]="parentForm" [formG]="formG" [field]="field" />
-      }
-      @case ("date") {
-        <app-date-picker [parentForm]="parentForm" [formG]="formG" [field]="field" />
-      }
-      @case ("search") {
-        <app-search [parentForm]="parentForm" [formG]="formG" [field]="field" />
-      }
-    }
-  `,
+  templateUrl: "./block-fields.component.html",
 })
 export class BlockFieldsComponent {
   @Input() parentForm!: FormGroup;
   @Input() formG!: FormGroup;
   @Input() field!: FormField;
+
+  getLabel(): string {
+    if (this.field?.label instanceof Function) {
+      return this.field.label(this.formG);
+    } else {
+      return this.field?.label || "";
+    }
+  }
 }
