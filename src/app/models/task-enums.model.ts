a1c4f5e47f3abb4260498f306747a1976ed48f96
DmitriyRusnak <@d.rusnak>
2026-05-12 17:38:08 +0500

    refactor(frontend): remove old model files and add comment-ext and task-enums

---
+++ src/app/models/task-enums.model.ts
@@ -0,0 +1,14 @@
+export enum PriorityTask {
+  LOW = "low",
+  MEDIUM = "medium",
+  HIGH = "high",
+  URGENT = "urgent",
+}
+
+export enum RepeatInterval {
+  NONE = "none",
+  DAILY = "daily",
+  WEEKLY = "weekly",
+  MONTHLY = "monthly",
+  YEARLY = "yearly",
+}
