7e098fb11090409d815fa8800abb36551a1c84e9
DmitriyRusnak <@d.rusnak>
2026-05-15 16:53:20 +0500

    refactor(frontend): update UI services, utilities, guards, models, and constants

--- src/app/services/ui/index.ts
+++ src/app/services/ui/index.ts
@@ -1,3 +1,4 @@
 export { DragDropOrderService } from "./drag-drop-order.service";
 export { ShortcutService } from "./shortcut.service";
 export { KanbanDragDropService } from "./kanban-drag-drop.service";
+export { ThemeService } from "./theme.service";
