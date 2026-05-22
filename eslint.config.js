8d18ef8b5badbc8980045577bb203de0f9e4608b
Dmitriy303 <rusnakdima03@gmail.com>
2026-05-22 12:56:11 +0500

    chore: format workflow and config files (android, eslint, package)

--- eslint.config.js
+++ eslint.config.js
@@ -35,10 +35,7 @@ module.exports = defineConfig([
   },
   {
     files: ["**/*.html"],
-    extends: [
-      angular.configs.templateRecommended,
-      angular.configs.templateAccessibility,
-    ],
+    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
     rules: {},
-  }
+  },
 ]);
