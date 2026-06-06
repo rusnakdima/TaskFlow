847187d4d94710daf72bd8164ff1a0b5d659c910
Dmitriy303 <rusnakdima03@gmail.com>
2026-06-07 01:55:39 +0500

    chore(tauri): update Cargo dependencies and format Rust code

--- src-tauri/src/helpers/qr.helper.rs
+++ src-tauri/src/helpers/qr.helper.rs
@@ -1,6 +1,23 @@
-/* Disabled QR code generation - moved to frontend to reduce build size */
+use base64::Engine;
+use image::Luma;
+use qrcode::QrCode;
 
-#[tauri::command]
-pub fn generate_qr_code_data_url(_data: &str) -> String {
-  String::new()
+pub fn generate_qr_code_data_url(data: &str) -> String {
+  let code = match QrCode::new(data.as_bytes()) {
+    Ok(c) => c,
+    Err(_) => return String::new(),
+  };
+
+  let image = code.render::<Luma<u8>>().build();
+
+  let mut png_data = Vec::new();
+  let mut cursor = std::io::Cursor::new(&mut png_data);
+
+  if let Err(_) = image.write_to(&mut cursor, image::ImageFormat::Png) {
+    return String::new();
+  }
+
+  let base64_str = base64::engine::general_purpose::STANDARD.encode(&png_data);
+
+  format!("data:image/png;base64,{}", base64_str)
 }
