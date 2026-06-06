7578643ab15ff9c89212336ce5f1c594cea5c8d2
Dmitriy303 <rusnakdima03@gmail.com>
2026-06-07 01:56:17 +0500

    style(form): format form section components (assignees, basic-info, block-fields, category, permissions, priority, template-form, timeline, visibility)

--- src/app/components/form/block-fields/block-fields.component.ts
+++ src/app/components/form/block-fields/block-fields.component.ts
@@ -2,37 +2,14 @@ import { Component, Input, ChangeDetectionStrategy } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormGroup } from "@angular/forms";
 
-import { FormField } from "@models/form-field.model";
+import { FormField, TypeField } from "@models/form-field.model";
 
-import { TextComponent } from "@components/fields/text/text.component";
-import { TextAreaComponent } from "@components/fields/text-area/text-area.component";
-import { NumberComponent } from "@components/fields/number/number.component";
-import { CheckboxComponent } from "@components/fields/checkbox/checkbox.component";
-import { SelectComponent } from "@components/fields/select/select.component";
-import { SlideToggleComponent } from "@components/fields/slide-toggle/slide-toggle.component";
-import { DatePickerComponent } from "@components/fields/date-picker/date-picker.component";
-import { ImageComponent } from "@components/fields/image/image.component";
-import { RadioComponent } from "@components/fields/radio/radio.component";
-import { SliderComponent } from "@components/fields/slider/slider.component";
-import { SliderRangeComponent } from "@components/fields/slider-range/slider-range.component";
+import { UnifiedFieldComponent } from "@components/fields/unified/unified-field.component";
 
 @Component({
   selector: "app-block-fields",
   standalone: true,
-  imports: [
-    CommonModule,
-    TextComponent,
-    NumberComponent,
-    TextAreaComponent,
-    ImageComponent,
-    DatePickerComponent,
-    SlideToggleComponent,
-    SelectComponent,
-    CheckboxComponent,
-    RadioComponent,
-    SliderComponent,
-    SliderRangeComponent,
-  ],
+  imports: [CommonModule, UnifiedFieldComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: "./block-fields.component.html",
 })
@@ -40,6 +17,8 @@ export class BlockFieldsComponent {
   @Input() parentForm!: FormGroup;
   @Input() formG!: FormGroup;
   @Input() field!: FormField;
+
+  TypeField = TypeField;
 
   getLabel(): string {
     if (this.field?.label instanceof Function) {
