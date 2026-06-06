29192048c19f71eb56d2d60f03583d2a3e78ca59
Dmitriy303 <rusnakdima03@gmail.com>
2026-06-07 01:56:06 +0500

    style(dialogs): format comments and confirm/prompt dialogs

--- src/app/components/confirm-dialog/confirm-dialog.component.ts
+++ src/app/components/confirm-dialog/confirm-dialog.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ConfirmDialogService } from "@services/core/confirm-dialog.service";
+import { AppButtonComponent } from "@components/shared/button/button.component";
 
 @Component({
   selector: "app-confirm-dialog",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AppButtonComponent],
   templateUrl: "./confirm-dialog.component.html",
 })
 export class ConfirmDialogComponent {
