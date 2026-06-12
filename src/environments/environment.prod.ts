08b53e83b2541e91c448b6d0c094d2b50f412dd1
Dmitriy303 <rusnakdima03@gmail.com>
2026-06-13 02:02:34 +0500

    feat(config): add production environment file

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,22 @@
+export const environment = {
+  production: true,
+  version: "0.18.0",
+  logEnabled: false,
+  logLevel: "error" as const,
+  logDebug: false,
+  logInfo: false,
+  logWarn: false,
+  logError: true,
+  gitRepoName: "TaskFlow",
+  githubUser: "TechCraft-Solutions",
+  nameProduct: "Task Flow",
+  yearCreate: 2025,
+  companyName: "TechCraft Solutions",
+  authors: [
+    {
+      name: "Dmitriy303",
+      email: "rusnakdima03@gmail.com",
+      url: "https://github.com/rusnakdima",
+    },
+  ],
+};
