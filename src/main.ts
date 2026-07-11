744573aab3c0daba7630099444891bf32a9c1a45
Dmitriy303 <rusnakdima03@gmail.com>
2026-07-12 02:19:03 +0500

    chore: add vitest setup, update main (TaskFlow)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import "@tauri-front/shared";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { appConfig } from "@app/app.config";
 import { App } from "@app/app";
-bootstrapApplication(App, appConfig).catch((_err) => {});
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [provideZoneChangeDetection(), ...appConfig.providers],
+}).catch((_err) => {});
