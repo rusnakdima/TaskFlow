2b608c5fdd8d882953364b7c41502e52d057b742
Dmitriy303 <rusnakdima03@gmail.com>
2026-07-26 21:02:45 +0500

    chore(TaskFlow): move @tauri-front/shared import to first position

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
+import "@tauri-front/shared";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "@app/app";
 import { appConfig } from "@app/app.config";
-
-import "@tauri-front/shared";
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
